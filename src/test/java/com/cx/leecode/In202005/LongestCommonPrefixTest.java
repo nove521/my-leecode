7846a5eb27588dc48cf7df048020c7fb8dca94f2
oldman <597800451@qq.com>
2020-05-08 21:50:18 +0800

    init

---
+++ src/test/java/com/cx/leecode/In202005/LongestCommonPrefixTest.java
@@ -0,0 +1,9 @@
+package com.cx.leecode.In202005;
+
+import junit.framework.TestCase;
+
+public class LongestCommonPrefixTest extends TestCase {
+
+    public void testLongestCommonPrefix() {
+    }
+}
